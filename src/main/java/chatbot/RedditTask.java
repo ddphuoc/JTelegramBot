cd6d9bbde74bd8407fcffb5745f2260860e43bb0
phuocdoan <ddphuoc@gmail.com>
2021-02-15 10:47:52 -0600

    Initial commit

---
+++ src/main/java/chatbot/RedditTask.java
@@ -0,0 +1,89 @@
+package chatbot;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.logging.SimpleFormatter;
+
+public class RedditTask extends TimerTask {
+
+    int scheduleTime = 700;
+    String pythonSource = "";
+    String pythonData = "";
+    private Consumer<Boolean> consumer;
+
+    public  RedditTask() {
+
+    }
+
+    public RedditTask(int scheduleTime, String pythonSource, String pythonData, Consumer<Boolean> consumer) {
+        this.scheduleTime = scheduleTime;
+        this.pythonSource = pythonSource;
+        this.pythonData = pythonData;
+        this.consumer = consumer;
+    }
+
+    @Override
+    public void run() {
+        Date date = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("HHmmss");
+        int checkTime = Integer.parseInt(dateFormat.format(date));
+        if(checkTime != scheduleTime) {
+            consumer.accept(false);
+            return;
+        }
+        System.out.println("Start Reddit at: "
+                + LocalDateTime.ofInstant(Instant.ofEpochMilli(scheduledExecutionTime()),
+                ZoneId.systemDefault()));
+        try {
+            RedditCall();
+            consumer.accept(true);
+        } catch (IOException | InterruptedException ioException) {
+            System.out.println(ioException.getMessage());
+            consumer.accept(false);
+        }
+        System.out.println("Complete Reddit at: "
+                + LocalDateTime.ofInstant(Instant.ofEpochMilli(scheduledExecutionTime()),
+                ZoneId.systemDefault()));
+    }
+
+    /// <summary>
+    /// Running Scripts
+    /// Go to src/ directory.
+    /// Create a praw.ini file with the following
+    /// [ClientSecrets]
+    /// client_id =< your client id>
+    /// client_secret=<your client secret>
+    /// user_agent=<your user agent>
+    /// Note that the title of this section, ClientSecrets, is important because ticker_counts.py will specifically look for that title in the praw.ini file.
+    /// Install required modules using pip install -r requirements.txt
+    /// Run ticker_counts.py first
+    /// Now run yfinance_analysis.py
+    /// You will be able to find your results in data/ directory.
+    /// </summary>
+    /// <returns></returns>
+    private void RedditCall() throws IOException, InterruptedException {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("/bin/bash", "-c", "python3 ticker_counts.py");
+        processBuilder.directory(new File(pythonSource));
+        Process process = processBuilder.start();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        bufferedReader.lines().forEach(System.out::println);
+        int exitCode = process.waitFor();
+
+        processBuilder = new ProcessBuilder();
+        processBuilder.command("/bin/bash", "-c", "python3 yfinance_analysis.py");
+        processBuilder.directory(new File(pythonSource));
+        process = processBuilder.start();
+        bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        bufferedReader.lines().forEach(System.out::println);
+        exitCode = process.waitFor();
+    }
+}
---
+++ src/main/java/chatbot/TelegramReddit.java
@@ -0,0 +1,225 @@
+package chatbot;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class TelegramReddit {
+
+    private static int collectTime = 700;
+    private static Hashtable<Long, String> htbFriends;
+    private static Hashtable<String, String> htbConfigs;
+    private static TelegramBot bot;
+    private static String friendListName = "friendList.txt";
+    private static String confFile = "conf.properties";
+    private static String pythonProgram = "";
+    private static String pythonData = "";
+    private static List<String> _lstMentions = new ArrayList<>();
+    private static  List<String> _lstBests = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        htbConfigs = new Hashtable<>();
+        htbFriends = new Hashtable<>();
+        confFile = args.length > 0 ? args[0]: confFile;
+        LoadConfigurations();
+        LoadFriends();
+
+        // Create your bot passing the token received from @BotFather
+        bot = new TelegramBot("1654024366:AAGFFapMC0bp445FPqBbeT5oe7WtaUahFzg");
+
+        // Register for updates
+        bot.setUpdatesListener(updates -> {
+            // ... process updates
+            for(Update update: updates){
+                // Send messages
+                long chatId = update.message().chat().id();
+                String msg = update.message().text();
+                if(msg.toLowerCase().equals("/redstock")) {
+                    if(!htbFriends.containsKey(chatId)) {
+                        AddFriend(chatId, update.message().from().firstName());
+                    }
+                } else if(msg.toLowerCase().equals("/redlatest")) {
+                    if(htbFriends.containsKey(chatId)) {
+                        SendMessageStock(chatId);
+                        SendMessageMention(chatId);
+                    }
+                }
+            }
+            // return id of last processed update or confirm them all
+            return UpdatesListener.CONFIRMED_UPDATES_ALL;
+        });
+        LoadBestStocks();
+        LoadMentions();
+
+        // Get Reddit stock everyday
+        new Timer().schedule(new RedditTask(collectTime, pythonProgram, pythonData, (successful) -> {
+            if(successful) {
+                try {
+                    LoadBestStocks();
+                    LoadMentions();
+                    SendMessageStock(-1);
+                    SendMessageMention(-1);
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+            }
+        }), 0, 10 * 59 * 1000);
+        System.out.println("System is running...");
+    }
+
+    private static void LoadConfigurations(){
+        try {
+            Path path = Files.createTempFile(confFile, "");
+            if(!Files.exists(path))
+                return;
+            // Load friend list
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(confFile));
+            bufferedReader.lines().forEach(line -> {
+                String [] w = line.split("=");
+                if(w.length == 2) {
+                    htbConfigs.put(w[0].trim(), w[1].trim());
+                }
+            });
+            bufferedReader.close();
+            if(htbConfigs.containsKey("friendListPath"))
+                friendListName = htbConfigs.get("friendListPath");
+            if(htbConfigs.containsKey("reddit.Schedule"))
+                collectTime = Integer.valueOf(htbConfigs.get("reddit.Schedule"));
+            if(htbConfigs.containsKey("reddit.python.program"))
+                pythonProgram = htbConfigs.get("reddit.python.program");
+            if(htbConfigs.containsKey("reddit.python.data"))
+                pythonData = htbConfigs.get("reddit.python.data");
+        } catch (Exception exception){
+
+        }
+    }
+
+    private static void LoadFriends(){
+        try {
+            Path path = Files.createTempFile(friendListName, "");
+            if(!Files.exists(path))
+                return;
+            // Load friend list
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(friendListName));
+            bufferedReader.lines().forEach(line -> {
+                String [] w = line.split("\t");
+                if(w.length == 2) {
+                    htbFriends.put(Long.valueOf(w[0].trim()), w[1].trim());
+                }
+            });
+            bufferedReader.close();
+        } catch (Exception exception){
+
+        }
+    }
+
+    private static void AddFriend(long chatId, String userName) {
+        try {
+            if(!htbFriends.containsKey(chatId)) {
+                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(friendListName, true));
+                bufferedWriter.write(String.format("%s\t%s", chatId, userName));
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                htbFriends.put (chatId, userName);
+                SendMessage(chatId, userName + " has been accepted !");
+                SendMessageStock(chatId);
+                SendMessageMention(chatId);
+            }
+        } catch (Exception exception) {
+            System.out.println("Add friend error: " + exception.getMessage());
+        }
+    }
+
+    private static void SendMessage(long chatId, String message){
+        try {
+            SendResponse response = bot.execute(new SendMessage(chatId, message));
+        } catch (Exception exception) {
+
+        }
+    }
+
+    private static void SendMessageMention(long chatId) {
+        if (_lstMentions.size() <= 0)
+            return;
+        String msg = "";
+        for (String stock:_lstMentions)
+        {
+            msg += stock + "\n";
+        }
+        for(long id:htbFriends.keySet()) {
+            if(chatId > 0 && id != chatId)
+                continue;
+            SendMessage(id, "Mention list");
+            SendMessage(id, msg);
+            SendMessage(id, "===== mention end =====");
+        }
+    }
+
+    private static void SendMessageStock(long chatId) {
+        if (_lstBests.size() <= 0)
+            return;
+        String msg = "";
+        for (String stock:_lstBests)
+        {
+            msg += stock + "\n";
+        }
+        for(long id:htbFriends.keySet()) {
+            if(chatId > 0 && id != chatId)
+                continue;
+            SendMessage(id, "Mention list");
+            SendMessage(id, msg);
+            SendMessage(id, "===== mention end =====");
+        }
+    }
+
+    private static void LoadMentions() throws IOException {
+        Date date = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String file = dateFormat.format(date) + "_tick_df.csv";
+        Path path = Paths.get(pythonData, file);
+
+        if(!Files.exists(path))
+            return;
+
+        _lstMentions.clear();
+        // Load friend list
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(path.toFile().getPath()));
+        bufferedReader.lines().forEach(line -> {
+            if(line.trim().length() > 0) {
+                _lstMentions.add(line);
+            }
+        });
+        bufferedReader.close();
+    }
+
+    private static void LoadBestStocks() throws IOException {
+        Date date = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String file = dateFormat.format(date) + "_df_best_25.csv";
+        Path path = Paths.get(pythonData, file);
+
+        if(!Files.exists(path))
+            return;
+
+        _lstBests.clear();
+        // Load friend list
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(path.toFile().getPath()));
+        bufferedReader.lines().forEach(line -> {
+            if(line.trim().length() > 0) {
+                _lstBests.add(line);
+            }
+        });
+        bufferedReader.close();
+    }
+}
